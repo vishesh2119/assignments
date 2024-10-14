727e7323214f5a7ca3c6747f6243ec1c446ebb01
vishesh2119 <101248335+vishesh2119@users.noreply.github.com>
2024-10-14 14:55:19 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,17 @@
+class helloworld{
+public static void main(String[] args){
+
+	int intval=21479343;
+	long longval=987973974937493749L;
+	float floatval=3.14f;
+	double doubleval=3.145464724;
+	char charval='A';
+	boolean boolval=true;
+	System.out.println("int value: "+intval);
+	System.out.println("long value: "+longval);
+	System.out.println("FLoat value: "+floatval);
+	System.out.println("doube value: "+doubleval);
+	System.out.println("char value: "+charval);
+	System.out.println("Boolean value: "+boolval);
+	}
+}
